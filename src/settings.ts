aac871737911e99b860c9d864dcc045263da5ad2
Maks <maksymnikomarov@gmail.com>
2024-01-20 21:16:22 +0000

    F

--- src/settings.ts
+++ src/settings.ts
@@ -1,12 +1,18 @@
 import express, { Request, Response } from 'express';
 import {blogRoute} from "./routes/blog-route";
 import {postRoute} from "./routes/post-route";
+import {db} from "./db/db";
 
 export const app = express();
 
 app.use(express.json());
 app.use('/blogs', blogRoute)
 app.use('/posts', postRoute)
+app.delete('/testing/all-data', (req: Request, res: Response) => {
+    db.posts.length = 0;
+    db.blogs.length = 0;
+    res.sendStatus(204);
+})
 // const availableResolution = ['P144', 'P240', 'P360', 'P480', 'P720', 'P1080', 'P1440', 'P2160'];
 //
 //  type VideoType = {
